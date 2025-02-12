6198434236cdac15667b959f175d782690711711
Guillaume Joyet <guillaume.joyet@unibas.ch>
2025-02-12 10:07:48 +0100

    Try AutoModel to load checkpoint.

--- src/train_gpt.py
+++ src/train_gpt.py
@@ -45,6 +45,7 @@ import numpy as np
 from encoder.conformer_braindecode import EEGConformer
 from torch import manual_seed
 from torch.utils.data import random_split, Subset
+from transformers import AutoModel
 import sys
 
 from utils import cv_split_bci, read_threshold_sub
@@ -139,34 +140,6 @@ def train(config: Dict = None) -> Trainer:
         manual_seed(config["seed"])
 
     # handles the input part, which are the output from encoder.
-
-    # TODO: check where config["training_style"] is used and see if I can just add a new training style.
-    # if config["training_style"] == 'decoding':  # decoding motion imagery
-    #     downstream_path = config["dst_data_path"]
-    #
-    #     # @Guillaume: change indexing below (from 0:18 to 1:19) to ignore file '.DS_Store' when running locally
-    #     train_folds, test_folds = cv_split_bci(sorted(os.listdir(downstream_path))[:18])
-    #     train_files = train_folds[config['fold_i']]
-    #     test_files = test_folds[config['fold_i']]
-    #
-    #     train_dataset = MotorImageryDataset(train_files, sample_keys=[
-    #         'inputs',
-    #         'attention_mask'
-    #     ], chunk_len=config["chunk_len"], num_chunks=config["num_chunks"], ovlp=config["chunk_ovlp"],
-    #                                         root_path=downstream_path, gpt_only=not config["use_encoder"])
-    #     # pdb.set_trace()
-    #
-    #     test_dataset = MotorImageryDataset(test_files, sample_keys=[
-    #         'inputs',
-    #         'attention_mask'
-    #     ], chunk_len=config["chunk_len"], num_chunks=config["num_chunks"], ovlp=config["chunk_ovlp"],
-    #                                        root_path=downstream_path, gpt_only=not config["use_encoder"])
-    #
-    #     validation_dataset = test_dataset
-    #     test_dataset = train_dataset
-
-    # TODO: leave-trial-out CV: len(dataset) // num_chunks -> select trials for test set
-    #           -> transform back to chunk indices instead of trial indices
     dataset = None
     if config["training_style"] == 'decoding':  # decoding 2AFC
         # For now, splits train/test set across all subjects.
@@ -292,6 +265,8 @@ def train(config: Dict = None) -> Trainer:
                 'model_final'
             )
         )
+    else:
+        trainer.model = AutoModel.from_pretrained(config["resume_from"])
 
     # GENERAL EVALUATION
     if test_dataset is not None and not os.path.isfile(os.path.join(
