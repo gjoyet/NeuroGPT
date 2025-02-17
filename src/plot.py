0c04afd1297afa598d45c016569d23f4da47150d
Guillaume Joyet <guillaume.joyet@unibas.ch>
2025-02-17 10:25:11 +0100

    Implement plotting of UMAP dataframes.

--- src/plot.py
+++ src/plot.py
@@ -12,6 +12,10 @@ matplotlib.use('macOSX')
 # TODO: test this (locally!)
 def plot_results(results_folder_path):
     models = os.listdir(results_folder_path)
+
+    pattern = r"^(not-pretrained-)?(trialCV|subjCV)(-partition\d+)?(-.*)?$"
+    models = [s for s in models if re.match(pattern, s)]
+
     models.sort()
 
     model_groups = defaultdict(list)
@@ -56,7 +60,48 @@ def plot_results(results_folder_path):
             # plot again, with hue (or whatever) set to 'group', one for training, one for test
 
 
+def plot_umap(results_folder_path):
+    filenames = os.listdir(results_folder_path)
+    umap_files = [f for f in filenames if 'umap' in f]
+
+    for uf in umap_files:
+        df = pd.read_csv(os.path.join(results_folder_path, uf))
+
+        for colouring in ['Subject ID', 'Label']:
+            # Create a seaborn lineplot, passing the matrix directly to seaborn
+            plt.figure(figsize=(10, 6))  # Optional: Set the figure size
+
+            # Create the lineplot, seaborn will automatically calculate confidence intervals
+            sns.scatterplot(data=df, x='x_embed', y='y_embed',
+                            hue=df[colouring].map({df[colouring].min(): 0, df[colouring].max(): 1}))
+            sns.despine()
+
+            # Set plot labels and title
+            plt.legend(title=colouring)
+
+            c = 'subjID' if colouring == 'Subject ID' else 'label'
+            plt.savefig(os.path.join('../results/', 'plots', f'{uf[:-4]}_{c}.png'))
+
+        # Create a seaborn lineplot, passing the matrix directly to seaborn
+        plt.figure(figsize=(10, 6))  # Optional: Set the figure size
+
+        # Create the lineplot, seaborn will automatically calculate confidence intervals
+        sns.scatterplot(data=df, x='x_embed', y='y_embed', hue='Label', style='Subject ID')
+        sns.despine()
+
+        # Set plot labels and title
+        plt.legend()
+
+        c = 'subjID' if colouring == 'Subject ID' else 'label'
+        plt.savefig(os.path.join('../results/', 'plots', f'{uf[:-4]}_combined.png'))
+
+
 if __name__ == '__main__':
-    print(os.getcwd())
-    results_folder = '../results/models/upstream'
+    results_folder = '/Users/joyet/Documents/Documents - Guillaume’s MacBook Pro/UniBasel/MSc_Data_Science/Master ' \
+                     'Thesis/Code/NeuroGPT/results/models/upstream'
+    if not os.path.isdir('/Users/joyet/Documents/Documents - Guillaume’s MacBook Pro/UniBasel/MSc_Data_Science/Master '
+                         'Thesis/Code/NeuroGPT/results/plots'):
+        os.mkdir('/Users/joyet/Documents/Documents - Guillaume’s MacBook Pro/UniBasel/MSc_Data_Science/Master '
+                 'Thesis/Code/NeuroGPT/results/plots')
     plot_results(results_folder_path=results_folder)
+    plot_umap(results_folder_path=os.path.join(results_folder, 'test-0'))
