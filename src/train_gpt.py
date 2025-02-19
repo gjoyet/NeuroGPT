7f4a58646b3efdfa726c1084839f02b467659330
Guillaume Joyet <guillaume.joyet@unibas.ch>
2025-02-19 10:49:05 +0100

    Change train and test dataset to be randomly split (no cross-validation, no fixed partitions).

--- src/train_gpt.py
+++ src/train_gpt.py
@@ -144,13 +144,11 @@ def train(config: Dict = None) -> Trainer:
     # handles the input part, which are the output from encoder.
     dataset = None
     if config["training_style"] == 'decoding':  # decoding 2AFC
-        # For now, splits train/test set across all subjects.
-        # Could be modified to include subjects only in one of both sets.
         downstream_path = config["dst_data_path"]
         filenames = sorted(os.listdir(downstream_path))
 
-        partition_dir = config["training_partition_loc"]
-        partition_id = config["training_partition_id"]
+        # partition_dir = config["training_partition_loc"]
+        # partition_id = config["training_partition_id"]
 
         if downstream_path.endswith('npz/'):
             dataset = CHBDataset_NPZ(filenames=filenames, sample_keys=[
@@ -169,22 +167,22 @@ def train(config: Dict = None) -> Trainer:
         else:
             raise ImportError('Issue with loading data.')
 
-        # # Split lengths (e.g., 80% train, 20% test)
-        # split = 0.8
-        # train_size = int(split * len(dataset))
-        # test_size = len(dataset) - train_size
+        # Split lengths (e.g., 80% train, 20% test)
+        split = 0.8
+        train_size = int(split * len(dataset))
+        test_size = len(dataset) - train_size
+
+        # Split the dataset
+        train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+        # partition = get_training_partition(savedir=partition_dir, dataset_size=len(dataset),
+        #                                    num_chunks=config["num_chunks"], partition_id=partition_id)
         #
-        # # Split the dataset
-        # train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
-
-        partition = get_training_partition(savedir=partition_dir, dataset_size=len(dataset),
-                                           num_chunks=config["num_chunks"], partition_id=partition_id)
-
-        training_indices = np.concatenate([p for i, p in enumerate(partition) if i != partition_id])
-        test_indices = partition[partition_id]
-
-        train_dataset = Subset(dataset, training_indices)
-        test_dataset = Subset(dataset, test_indices)
+        # training_indices = np.concatenate([p for i, p in enumerate(partition) if i != partition_id])
+        # test_indices = partition[partition_id]
+        #
+        # train_dataset = Subset(dataset, training_indices)
+        # test_dataset = Subset(dataset, test_indices)
 
         validation_dataset = test_dataset
         test_dataset = train_dataset
