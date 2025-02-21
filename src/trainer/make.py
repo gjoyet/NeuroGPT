c45a5750dc67fd39b9582b91ed30de3b3ebeb464
Guillaume Joyet <guillaume.joyet@unibas.ch>
2025-02-21 15:14:56 +0100

    Debug.

--- src/trainer/make.py
+++ src/trainer/make.py
@@ -87,7 +87,7 @@ def _cat_data_collator(features: List) -> Dict[str, torch.tensor]:
 
 def decoding_accuracy_metrics(eval_preds):
     preds, labels = eval_preds  # @Guillaume: will maybe need to reshape stuff
-    preds = preds.view(-1)
+    preds = preds.reshape(-1)
     # preds = preds.argmax(axis=-1)
     mse = mean_squared_error(labels, preds)
     mae = mean_absolute_error(labels, preds)
