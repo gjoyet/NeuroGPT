6ff09f84f5fa06753a62404761086892c2e6154c
Guillaume Joyet <guillaume.joyet@unibas.ch>
2025-02-25 16:16:48 +0100

    Change dimensions of final layer.

--- src/encoder/conformer_braindecode.py
+++ src/encoder/conformer_braindecode.py
@@ -393,10 +393,10 @@ class _FullyConnected(nn.Module):
             nn.Linear(256, 256),
             nn.ELU(),
             nn.Dropout(drop_prob_1),
-            nn.Linear(256, 128),
+            nn.Linear(256, 64),
             nn.ELU(),
             nn.Dropout(drop_prob_1),
-            nn.Linear(128, hidden_channels),
+            nn.Linear(64, hidden_channels),
             nn.ELU(),
             # nn.Dropout(drop_prob_2),
         )
