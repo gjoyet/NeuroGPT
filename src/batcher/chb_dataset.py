ff9e15db13825daebeca1c17afe599870b3cf617
Guillaume Joyet <guillaume.joyet@unibas.ch>
2025-02-25 09:16:29 +0100

    Scale up label (x10) to try to combat underfitting.

--- src/batcher/chb_dataset.py
+++ src/batcher/chb_dataset.py
@@ -99,4 +99,4 @@ class CHBDataset_HDF5(EEGDataset):
         trial = self.files[file_index]['epochs'][infile_trial_index, :, select:select + self.chunk_len]
         label = self.files[file_index]['labels'][infile_trial_index, chunk_index]
 
-        return self.preprocess_sample(np.array(trial), 1, np.array(label))
+        return self.preprocess_sample(np.array(trial), 1, 10 * np.array(label))
