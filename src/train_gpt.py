f0a414963866b6a379b3631542fbb17f9b14e642
Guillaume Joyet <guillaume.joyet@unibas.ch>
2025-03-03 16:25:50 +0100

    Correction of original code.

--- src/train_gpt.py
+++ src/train_gpt.py
@@ -265,11 +265,11 @@ def train(config: Dict = None) -> Trainer:
         #            ))
 
     # GENERAL EVALUATION
-    if test_dataset is not None and not os.path.isfile(os.path.join(
+    if validation_dataset is not None and not os.path.isfile(os.path.join(
             config["log_dir"],
             'test_metrics.csv'
     )):
-        test_prediction = trainer.predict(test_dataset)
+        test_prediction = trainer.predict(validation_dataset)
         pd.DataFrame(
             test_prediction.metrics,
             index=[0]
