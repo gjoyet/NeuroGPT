267334e9e393e5903b711aa9642cd6fe35017726
Guillaume Joyet <guillaume.joyet@unibas.ch>
2025-03-07 13:21:17 +0100

    Debug.

--- src/train_gpt.py
+++ src/train_gpt.py
@@ -181,11 +181,11 @@ def train(config: Dict = None) -> Trainer:
         training_indices = np.concatenate([p for i, p in enumerate(partition) if i != partition_id])
         test_indices = partition[partition_id]
 
-        # keep more of the samples that are closer to response
-        training_indices = training_indices[np.isin(training_indices % config["num_chunks"] - config["num_chunks"],
-                                                    [-1, -2, -3, -5, -9, -config["num_chunks"]+1])]
+        # train mainly on timesteps that are closer to response
+        sel_training_indices = training_indices[np.isin(training_indices % config["num_chunks"] - config["num_chunks"],
+                                                        [-1, -2, -3, -5, -9, -config["num_chunks"]+1])]
 
-        train_dataset = Subset(dataset, training_indices)
+        train_dataset = Subset(dataset, sel_training_indices)
         test_dataset = Subset(dataset, test_indices)
 
         validation_dataset = test_dataset
@@ -300,6 +300,7 @@ def train(config: Dict = None) -> Trainer:
         )
 
     # TIME-DEPENDENT EVALUATION (training and test sets)
+    train_dataset = Subset(dataset, training_indices)  # train_dataset contains all timesteps again
     for setting, ds in zip(['training', 'test'], [train_dataset, validation_dataset]):
         output_path = os.path.join(
             config["log_dir"],
