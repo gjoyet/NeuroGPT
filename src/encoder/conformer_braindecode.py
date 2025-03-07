046c064e939812f70b9e628286dfc8b166334bbb
Guillaume Joyet <guillaume.joyet@unibas.ch>
2025-03-07 11:36:34 +0100

    Debug.

--- src/encoder/conformer_braindecode.py
+++ src/encoder/conformer_braindecode.py
@@ -171,6 +171,9 @@ class EEGConformer(EEGModuleMixin, nn.Module):
         if self.is_decoding_mode:
             # pdb.set_trace()
             for i in range(batch):
+                print(x.shape)
+                print(x[i].unsqueeze(0).shape)
+                print(self.fc_list[subjects[0]](x[0].unsqueeze(0)).shape)
                 x = torch.cat([self.fc_list[subjects[i]](x[i].unsqueeze(0)) for i in range(batch)], dim=0)
                 x = torch.cat([self.final_layer_list[subjects[i]](x[i].unsqueeze(0)) for i in range(batch)], dim=0)
         return x
